53c6cc10096ea08ab87f2e31f59d2c15eb1ae848
Erdem Uslu <usluerdem@yandex.com>
2020-04-19 17:17:10 +0300

    update :: style changes for savedItemsView

--- src/View.js
+++ src/View.js
@@ -2,14 +2,7 @@ class View {
   constructor() {
     this.root = document.getElementById('root');
 
-    this.header = document.createElement('header');
-    this.headerTitle = document.createElement('h1');
-    this.headerTitle.innerText = 'Is Ilanlari';
-    this.headerBookmarkItem = document.createElement('div');
-    this.headerBookmarkItem.setAttribute('class', 'saved-items');
-    this.header.append(this.headerTitle);
-    this.header.append(this.headerBookmarkItem);
-    this.root.append(this.header);
+    this.createHeaderView();
 
     this.list = document.createElement('div');
     this.list.setAttribute('class', 'list');
@@ -19,9 +12,44 @@ class View {
     this.loadingSection.innerText = 'Loading...';
 
     this.root.append(this.loadingSection);
+
+    this.handleSave = this.handleSave.bind(this);
   }
 
-  createListItem(item, index) {
+  createHeaderView() {
+    this.header = document.createElement('header');
+
+    this.headerLogoWrapper = document.createElement('div');
+    this.headerLogoWrapper.classList.add('header-logo');
+    this.headerTitle = document.createElement('h1');
+    this.headerTitle.innerText = 'Is Ilanlari';
+    this.headerLogoWrapper.append(this.headerTitle);
+
+    this.headerCtaWrapper = document.createElement('div');
+    this.headerCtaWrapper.classList.add('header-cta');
+    this.headerCtaButton = document.createElement('button');
+    this.headerCtaButton.innerText = 'Kaydedilen Is Ilanlari';
+    this.headerCtaButton.addEventListener('click', this.showSavedItems.bind(this));
+    this.headerCtaCountEl = document.createElement('span');
+    this.headerCtaCountEl.innerText = '0';
+    this.headerCtaButton.append(this.headerCtaCountEl);
+    this.headerCtaWrapper.append(this.headerCtaButton);
+
+    this.headerBookmarkItem = document.createElement('div');
+    this.headerBookmarkItem.setAttribute('class', 'saved-items');
+
+    this.header.append(this.headerLogoWrapper);
+    this.header.append(this.headerCtaWrapper);
+    this.header.append(this.headerBookmarkItem);
+
+    this.root.append(this.header);
+  }
+
+  showSavedItems() {
+    this.headerBookmarkItem.classList.add('is-show');
+  }
+
+  createListItem(item, index, type = 'save') {
     const listItem = document.createElement('div');
     listItem.setAttribute('class', 'list-item');
 
@@ -36,9 +64,9 @@ class View {
     listItemButton.setAttribute('href', item.url);
 
     const listItemSaveButton = document.createElement('button');
-    listItemSaveButton.innerText = 'Kaydet';
+    listItemSaveButton.innerText = type === 'save' ? 'Kaydet' : 'Sil';
     listItemSaveButton.id = index;
-    listItemSaveButton.addEventListener('click', this.handleSave.bind(this));
+    listItemSaveButton.addEventListener('click', type === 'save' ? this.handleSave : this.handleRemove, true);
 
     listItem.append(listItemTitle);
     listItem.append(listItemCompany);
@@ -46,6 +74,10 @@ class View {
     listItem.append(listItemSaveButton);
 
     return listItem;
+  }
+
+  handleRemove(e) {
+    console.log('handleRemove', e);
   }
 
   handleSaveAction(func) {
@@ -57,8 +89,13 @@ class View {
   }
 
   onSavedDataChanged(savedItem, id) {
-    const savedItemEl = this.createListItem(savedItem, id);
-    this.header.append(savedItemEl);
+    const targetButton = document.querySelector(`.list .list-item button[id="${id}"]`);
+    targetButton.removeEventListener('click', this.handleSave, true);
+    targetButton.innerText = 'Sil';
+    targetButton.addEventListener('click', this.handleRemove);
+
+    const savedItemEl = this.createListItem(savedItem, id, 'remove');
+    this.headerBookmarkItem.append(savedItemEl);
   }
 
   createList(jobs) {
